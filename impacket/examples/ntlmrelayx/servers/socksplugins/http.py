f248d9cdc125dc37e2b3e62c9067c9f705b18440
sender <me@allendemoura.com>
2025-07-17 01:08:03 -0700

    Add comprehensive error handling and input validation
    - Add connection error handling for client socket operations
    - Add validation for empty data from clients
    - Improve header parsing with Unicode error handling
    - Add defensive parsing to prevent crashes on malformed headers
    - Add connection health checks before critical operations
    - Add comprehensive error handling in transferResponse()
    - Add robust chunked transfer error handling with proper validation
    - Add error handling in prepareRequest() for body reading
    - Add error handling in tunnelConnection() for all operations
    - Prevent broken pipe errors and cascading failures
    - Graceful handling of connection drops and malformed data

--- impacket/examples/ntlmrelayx/servers/socksplugins/http.py
+++ impacket/examples/ntlmrelayx/servers/socksplugins/http.py
@@ -61,7 +61,14 @@ class HTTPSocksRelay(SocksRelay):
 
     def skipAuthentication(self):
         # See if the user provided authentication
-        data = self.socksSocket.recv(self.packetSize)
+        try:
+            data = self.socksSocket.recv(self.packetSize)
+            if not data:
+                LOG.debug('HTTP: No data received from client')
+                return False
+        except (ConnectionResetError, BrokenPipeError, OSError) as e:
+            LOG.debug('HTTP: Client connection error: %s' % str(e))
+            return False
         # Get headers from data
         headerDict = self.getHeaders(data)
         try:
@@ -107,71 +114,159 @@ class HTTPSocksRelay(SocksRelay):
         # Point our socket to the sock attribute of HTTPConnection
         # (contained in the session), which contains the socket
         self.relaySocket = self.session.sock
+        
+        # Check if connection is still alive
+        if not self.isConnectionAlive():
+            LOG.error('HTTP: Relay connection is dead for session %s' % self.username)
+            return False
+            
         # Send the initial request to the server
-        tosend = self.prepareRequest(data)
-        self.relaySocket.send(tosend)
-        # Send the response back to the client
-        self.transferResponse()
-        return True
+        try:
+            tosend = self.prepareRequest(data)
+            self.relaySocket.send(tosend)
+            # Send the response back to the client
+            self.transferResponse()
+            return True
+        except (ConnectionResetError, BrokenPipeError, OSError) as e:
+            LOG.error('HTTP: Failed to send initial request for session %s: %s' % (self.username, str(e)))
+            return False
 
     def getHeaders(self, data):
         # Get the headers from the request, ignore first "header"
         # since this is the HTTP method, identifier, version
         headerSize = data.find(EOL+EOL)
         headers = data[:headerSize].split(EOL)[1:]
-        headers = [header.decode("ascii") for header in headers]
-        headerDict = {hdrKey.split(':')[0].lower():hdrKey.split(':', 1)[1][1:] for hdrKey in headers}
+        headerDict = {}
+        for header in headers:
+            try:
+                hdrKey = header.decode("ascii")
+                if ':' in hdrKey:
+                    parts = hdrKey.split(':', 1)
+                    if len(parts) == 2:
+                        headerDict[parts[0].lower()] = parts[1][1:]  # Remove leading space
+            except UnicodeDecodeError:
+                # Skip headers with non-ASCII characters
+                continue
         return headerDict
 
     def transferResponse(self):
-        data = self.relaySocket.recv(self.packetSize)
-        headerSize = data.find(EOL+EOL)
-        headers = self.getHeaders(data)
         try:
-            bodySize = int(headers['content-length'])
-            readSize = len(data)
-            # Make sure we send the entire response, but don't keep it in memory
-            self.socksSocket.send(data)
-            while readSize < bodySize + headerSize + 4:
-                data = self.relaySocket.recv(self.packetSize)
-                readSize += len(data)
+            data = self.relaySocket.recv(self.packetSize)
+            if not data:
+                LOG.debug('HTTP: No data received from relay socket - connection may be closed')
+                return
+                
+            headerSize = data.find(EOL+EOL)
+            if headerSize == -1:
+                LOG.debug('HTTP: No complete headers found in response')
                 self.socksSocket.send(data)
-        except KeyError:
+                return
+                
+            headers = self.getHeaders(data)
             try:
-                if headers['transfer-encoding'] == 'chunked':
-                    # Chunked transfer-encoding, bah
-                    LOG.debug('Server sent chunked encoding - transferring')
-                    self.transferChunked(data, headers)
-                else:
-                    # No body in the response, send as-is
+                bodySize = int(headers.get('content-length', 0))
+                if bodySize > 0:
+                    readSize = len(data)
+                    # Make sure we send the entire response, but don't keep it in memory
                     self.socksSocket.send(data)
-            except KeyError:
-                # No body in the response, send as-is
+                    while readSize < bodySize + headerSize + 4:
+                        try:
+                            data = self.relaySocket.recv(self.packetSize)
+                            if not data:
+                                LOG.debug('HTTP: Connection closed while reading body')
+                                break
+                            readSize += len(data)
+                            self.socksSocket.send(data)
+                        except (ConnectionResetError, BrokenPipeError, OSError) as e:
+                            LOG.debug('HTTP: Connection error while reading response body: %s' % str(e))
+                            break
+                else:
+                    # No content-length, check for chunked encoding
+                    if headers.get('transfer-encoding', '').lower() == 'chunked':
+                        # Chunked transfer-encoding
+                        LOG.debug('Server sent chunked encoding - transferring')
+                        self.transferChunked(data, headers)
+                    else:
+                        # No body in the response, send as-is
+                        self.socksSocket.send(data)
+            except (ValueError, KeyError):
+                # Error parsing content-length or other header issues
+                LOG.debug('HTTP: Error parsing response headers, sending as-is')
                 self.socksSocket.send(data)
+        except (ConnectionResetError, BrokenPipeError, OSError) as e:
+            LOG.debug('HTTP: Socket error in transferResponse: %s' % str(e))
+            # Don't re-raise, let the caller handle the connection cleanup
 
-    def transferChunked(self, data, headers):
-        headerSize = data.find(EOL+EOL)
+    def transferChunked(self, data, _headers):
+        try:
+            headerSize = data.find(EOL+EOL)
+            if headerSize == -1:
+                LOG.debug('HTTP: Invalid chunked response - no headers')
+                return
 
-        self.socksSocket.send(data[:headerSize + 4])
+            self.socksSocket.send(data[:headerSize + 4])
 
-        body = data[headerSize + 4:]
-        # Size of the chunk
-        datasize = int(body[:body.find(EOL)], 16)
-        while datasize > 0:
-            # Size of the total body
-            bodySize = body.find(EOL) + 2 + datasize + 2
-            readSize = len(body)
-            # Make sure we send the entire response, but don't keep it in memory
-            self.socksSocket.send(body)
-            while readSize < bodySize:
-                maxReadSize = bodySize - readSize
-                body = self.relaySocket.recv(min(self.packetSize, maxReadSize))
-                readSize += len(body)
+            body = data[headerSize + 4:]
+            if not body:
+                LOG.debug('HTTP: No body data for chunked response')
+                return
+                
+            # Size of the chunk
+            try:
+                eol_pos = body.find(EOL)
+                if eol_pos == -1:
+                    LOG.debug('HTTP: Invalid chunk size format')
+                    return
+                datasize = int(body[:eol_pos], 16)
+            except ValueError:
+                LOG.debug('HTTP: Cannot parse chunk size')
+                return
+                
+            while datasize > 0:
+                try:
+                    # Size of the total body
+                    bodySize = body.find(EOL) + 2 + datasize + 2
+                    readSize = len(body)
+                    # Make sure we send the entire response, but don't keep it in memory
+                    self.socksSocket.send(body)
+                    while readSize < bodySize:
+                        maxReadSize = bodySize - readSize
+                        try:
+                            body = self.relaySocket.recv(min(self.packetSize, maxReadSize))
+                            if not body:
+                                LOG.debug('HTTP: Connection closed during chunked transfer')
+                                return
+                            readSize += len(body)
+                            self.socksSocket.send(body)
+                        except (ConnectionResetError, BrokenPipeError, OSError) as e:
+                            LOG.debug('HTTP: Connection error during chunk read: %s' % str(e))
+                            return
+                    
+                    try:
+                        body = self.relaySocket.recv(self.packetSize)
+                        if not body:
+                            LOG.debug('HTTP: Connection closed while reading next chunk')
+                            return
+                        eol_pos = body.find(EOL)
+                        if eol_pos == -1:
+                            datasize = 0  # Exit loop if no EOL found
+                        else:
+                            datasize = int(body[:eol_pos], 16)
+                    except (ValueError, ConnectionResetError, BrokenPipeError, OSError) as e:
+                        LOG.debug('HTTP: Error reading chunk size: %s' % str(e))
+                        return
+                except Exception as e:
+                    LOG.debug('HTTP: Error in chunked transfer loop: %s' % str(e))
+                    return
+                    
+            LOG.debug('Last chunk received - exiting chunked transfer')
+            try:
                 self.socksSocket.send(body)
-            body = self.relaySocket.recv(self.packetSize)
-            datasize = int(body[:body.find(EOL)], 16)
-        LOG.debug('Last chunk received - exiting chunked transfer')
-        self.socksSocket.send(body)
+            except (ConnectionResetError, BrokenPipeError, OSError) as e:
+                LOG.debug('HTTP: Error sending final chunk: %s' % str(e))
+                
+        except Exception as e:
+            LOG.debug('HTTP: Unexpected error in transferChunked: %s' % str(e))
 
     def prepareRequest(self, data):
         # Parse the HTTP data, removing headers that break stuff
@@ -191,21 +286,33 @@ class HTTPSocksRelay(SocksRelay):
             response.append(part)
         # Append the body
         response.append(b'')
-        response.append(data.split(EOL+EOL)[1])
+        body_parts = data.split(EOL+EOL)
+        if len(body_parts) > 1:
+            response.append(body_parts[1])
+        else:
+            response.append(b'')  # No body for GET requests
         senddata = EOL.join(response)
 
         # Check if the body is larger than 1 packet
         headerSize = data.find(EOL+EOL)
         headers = self.getHeaders(data)
         try:
-            bodySize = int(headers['content-length'])
-            readSize = len(data)
-            while readSize < bodySize + headerSize + 4:
-                data = self.socksSocket.recv(self.packetSize)
-                readSize += len(data)
-                senddata += data
-        except KeyError:
-            # No body, could be a simple GET or a POST without body
+            bodySize = int(headers.get('content-length', 0))
+            if bodySize > 0:
+                readSize = len(data)
+                while readSize < bodySize + headerSize + 4:
+                    try:
+                        additional_data = self.socksSocket.recv(self.packetSize)
+                        if not additional_data:
+                            LOG.debug('HTTP: Client closed connection while reading request body')
+                            break
+                        readSize += len(additional_data)
+                        senddata += additional_data
+                    except (ConnectionResetError, BrokenPipeError, OSError) as e:
+                        LOG.debug('HTTP: Connection error while reading request body: %s' % str(e))
+                        break
+        except (KeyError, ValueError):
+            # No body or invalid content-length, could be a simple GET or a POST without body
             # no need to check if we already have the full packet
             pass
         return senddata
@@ -213,12 +320,21 @@ class HTTPSocksRelay(SocksRelay):
 
     def tunnelConnection(self):
         while True:
-            data = self.socksSocket.recv(self.packetSize)
-            # If this returns with an empty string, it means the socket was closed
-            if data == '':
+            try:
+                data = self.socksSocket.recv(self.packetSize)
+                # If this returns with an empty string, it means the socket was closed
+                if not data:
+                    LOG.debug('HTTP: Client closed connection')
+                    return
+                    
+                # Pass the request to the server
+                tosend = self.prepareRequest(data)
+                self.relaySocket.send(tosend)
+                # Send the response back to the client
+                self.transferResponse()
+            except (ConnectionResetError, BrokenPipeError, OSError) as e:
+                LOG.debug('HTTP: Connection error in tunnel: %s' % str(e))
                 return
-            # Pass the request to the server
-            tosend = self.prepareRequest(data)
-            self.relaySocket.send(tosend)
-            # Send the response back to the client
-            self.transferResponse()
+            except Exception as e:
+                LOG.debug('HTTP: Unexpected error in tunnel: %s' % str(e))
+                return
